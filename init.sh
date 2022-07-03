e806bcf5440daa5095388c2358266013fc34ab04
Falldot <fe.offep@gmail.com>
2022-07-03 11:15:20 +1100

    add main sh script

---
+++ init.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+AUR=aur.archlinux.org
+
+pacman -Syu
+
+echo '[MY_SCRIPT] Установка alacritty fish wofi htop'
+sudo pacman -S alacritty fish wofi htop
+
+sudo mkdir -rf $HOME/$AUR
+
+echo '[MY_SCRIPT] Установка yay'
+sudo pacman -S git
+cd $HOME/$AUR && git clone https://$AUR/yay.git && cd yay && makepkg -si 
+yay -Y --gendb
+
+echo '[MY_SCRIPT] Sway'
+yay -S wayland polkit sway swaybg swaylock swaylock-effects-git swayidle-git waybar-git otf-font-awesome mako-git grim-git slurp-git
+
+echo '[MY_SCRIPT] Установка Fira Code шрифт'
+yay -S ttf-fira-code nerd-fonts-fira-code
+
+echo '[MY_SCRIPT] Pipeware'
+yay -S pipewire pipewire-alsa pipewire-pulse pipewire-jack pw-volume
+
+echo '[MY_SCRIPT] Sway'
+rm -rf $HOME/.config/sway
+ln -s $(pwd)/.config/sway $HOME/.config
+
+echo '[MY_SCRIPT] Waybar'
+rm -rf $HOME/.config/waybar
+ln -s $(pwd)/.config/waybar $HOME/.config
+
+echo '[MY_SCRIPT] Alacritty'
+rm -rf $HOME/.config/alacritty
+ln -s $(pwd)/.config/alacritty $HOME/.config
+
+echo '[MY_SCRIPT] Fish'
+chsh -s /usr/bin/fish
+rm -rf $HOME/.config/fish
+ln -s $(pwd)/.config/fish $HOME/.config
+
+sudo pacman -Sc
