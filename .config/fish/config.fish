633d67f6caefeda3ccbb9846283e90135532d135
Falldot <fe.offep@gmail.com>
2026-01-11 18:10:52 +1100

    Start

---
+++ .config/fish/config.fish
@@ -0,0 +1,71 @@
+function fish_greeting
+    # fastfetch
+end
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
+end
+
+## Functions
+# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | grep -e '^\.'"                                   # show only dotfiles
+
+# Common use
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# Recent installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
+# Packaging
+alias add="paru -Sy"
+alias remove="paru -Rns"
+alias upgrade="paru -Syu"
+alias cleanup="paru -Rns (paru -Qtdq)"
+alias history="builtin history --show-time='%F %T '"
---
+++ README.md
@@ -0,0 +1,10 @@
+# My Dotfiles
+
+## Install
+
+```sh
+sudo pacman -Sy git stow
+git clone https://github.com/Falldot/dotfiles ~/github.com/Falldot/dotfiles
+cd ~/github.com/Falldot/dotfiles
+stow . -t $HOME
+```
