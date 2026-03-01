1fadf30a8b41a30de539e093517358a410edfb5e
Falldot <fe.offep@gmail.com>
2026-03-01 16:31:11 +1100

    [nvim] Add config file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,7 @@
+vim.opt.number 					= true
+vim.opt.relativenumber 	= true
+vim.opt.tabstop 				= 4
+vim.opt.expandtab 			= true
+vim.opt.termguicolors 	= true
+
+vim.api.nvim_set_hl(0, "Normal", {})
